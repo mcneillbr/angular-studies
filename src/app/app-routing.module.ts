0df997ae8344f2256965895f6259dfbe241af73f
mcneillbr <mcneill.marcelo@yahoo.com.br>
2019-10-06 13:56:46 -0300

    adjustment and corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -46,7 +46,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes, { enableTracing: fa })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
